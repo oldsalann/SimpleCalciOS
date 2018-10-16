efe7213dcdbb2653e01da06965ccd0f89008ee6e
Nicholas Olds <oldsn@Nicholass-MacBook-Pro.local>
2018-10-16 14:12:20 -0700

    added ui

--- SimpleCalciOS/SimpleCalciOS/ViewController.swift
+++ SimpleCalciOS/SimpleCalciOS/ViewController.swift
@@ -10,6 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var userIsTyping = false
+    var count = 0
+    @IBOutlet weak var lblDisplay: UILabel!
+    
+    @IBAction func btnClickCalc(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+        count = count + 1
+        if (!userIsTyping) {
+            lblDisplay.text = digit
+            userIsTyping = true
+        } else {
+            let textInDisplay = lblDisplay.text!
+            lblDisplay.text = textInDisplay + digit
+        }
+    }
+    
+    var displayValue: Int {
+        get {
+            return Int(lblDisplay.text!)!
+        }
+        set {
+            lblDisplay.text! = String(newValue)
+        }
+    }
+    @IBAction func btnPerform(_ sender: UIButton) {
+        if userIsTyping {
+            brain.setOperand(displayValue)
+            userIsTyping = false;
+        }
+        if let mathSymbol = sender.currentTitle {
+            brain.performOp(mathSymbol)
+        }
+        if let result = brain.result {
+            displayValue = result
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
